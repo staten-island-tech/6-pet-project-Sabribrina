6b5332147e25264a737988ea07e0826f48186297
Sabribrina <sabrinal117@nycstudents.net>
2026-03-16 10:17:00 -0400

    3/16/26

--- woofwoof.py
+++ woofwoof.py
@@ -19,3 +19,29 @@ class Hero:
         self.inventory = inventory
 
     def buy (self, item):
+        self.inventory.append(item)
+        print(self.inventory)
+
+""" Jillian = Hero("Jillian", 150, ["Potion"])
+Jillian.buy({"title": "sword", "atk": 34})
+print(Jillian.__dict__ ) """
+
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.__balance = balance      # double underscore means "private"
+        
+    def deposit(self, amount):
+        self.__balance += amount
+    
+    def show_balance(self):
+        print(f"{self.owner} has ${self.__balance}")
+
+Yaotose_Hinako = Hero("Yaotose Hinako", 500, ["Yashiro Miko"])
+Yaotose_Hinako.buy({"title": "Oumi Shiori (Mermaid)", "atk": 999999})
+print(Yaotose_Hinako.__dict__)
+
+class Pet:
+    def __init__ (self, name, happiness):
+        self.name = name
+        self.happiness = happiness
