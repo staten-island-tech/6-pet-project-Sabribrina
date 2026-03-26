da4ea4bd9e595e12eef1adebf2b481ca96023b38
Sabribrina <sabrinal117@nycstudents.net>
2026-03-26 10:10:40 -0400

    3/26/26

--- magicalrrock.py
+++ magicalrrock.py
@@ -1,13 +1,13 @@
 def vendor (number, weight):
     total = 0
-    my_list = []
+    my_list = 0
     for i in range (number):
-        if i % 2:
-            total += i
-            my_list.append(total)
-        else:
+        if weight[i] % 2:
+            total += weight[i]
+            my_list = total
+            if my_list < total:
+                my_list = total
+        elif weight[i] % 2 != 0:
             total = 0
     print(my_list)
-    print(max(my_list))
-
 vendor(13, [2, 3, 4, 4, 5, 6, 1, 2, 2, 2, 1, 8, 2])
